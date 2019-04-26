445e6372dbcd448e0482b37407f49b01a0423d09
SunSpike <wjdehdgud321@naver.com>
2019-04-26 12:23:29 +0900

    Solved Domino Pilling

---
+++ src/codeForces/A_50.java
@@ -0,0 +1,14 @@
+package codeForces;
+
+import java.util.Scanner;
+
+public class A_50 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		
+		System.out.println((m*n)/2);
+	}
+}
